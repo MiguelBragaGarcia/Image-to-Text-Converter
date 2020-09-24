65d85db15c1414875fd029fbf85609f706b74121
Miguel Braga Garcia <miguelbragagarcia@gmail.com>
2020-09-24 19:45:57 -0300

    Adicionando validação as rotas

--- server/src/routes/process.routes.ts
+++ server/src/routes/process.routes.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import ProcessImageService from 'services/ProcessImageService';
 import multer from 'multer';
+import { celebrate, Joi, Segments } from 'celebrate';
+import ensureFileHasUploaded from 'middlewares/ensureFileHasUploaded';
 import uplaodConfig from '../config/upload';
 
 const processRouter = Router();
@@ -8,12 +10,25 @@ const processRouter = Router();
 const processImage = new ProcessImageService();
 const upload = multer(uplaodConfig.multer);
 
-processRouter.post('/', upload.single('image'), async (request, response) => {
-  const convertedText = await processImage.execute({
-    filename: request.file.filename,
-  });
+processRouter.post(
+  '/',
+  upload.single('image'),
+  ensureFileHasUploaded,
+  celebrate({
+    [Segments.BODY]: {
+      language: Joi.string().required(),
+    },
+  }),
+  async (request, response) => {
+    const { language } = request.body;
 
-  return response.json(convertedText);
-});
+    const convertedText = await processImage.execute({
+      filename: request.file.filename,
+      language,
+    });
+
+    return response.json(convertedText);
+  },
+);
 
 export default processRouter;
