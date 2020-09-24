9eb91769ef7e054a77b7a23fa4ca112c8a445513
Miguel Braga Garcia <miguelbragagarcia@gmail.com>
2020-09-24 19:47:01 -0300

    Configurando a aplicação para lidar com os erros de forma assincrona

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,32 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 
 import routes from 'routes';
+import 'express-async-errors';
+import { errors } from 'celebrate';
+import AppError from 'errors/AppError';
 
 const server = express();
 server.use(express.json());
 
 server.use(routes);
+
+server.use(errors());
+
+server.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: 'error',
+        message: err.message,
+      });
+    }
+    console.error(err);
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+);
 
 server.listen(3333, () => {
   console.log('O servidor iniciou no seguinte endereço http://localhost:3333');
