859c19164adbc637315ff9f28e89f9ae7040952d
Miguel Braga Garcia <miguelbragagarcia@gmail.com>
2020-09-24 19:48:40 -0300

    Adicionando opção de escolha de linguagem

--- server/src/services/ProcessImageService.ts
+++ server/src/services/ProcessImageService.ts
@@ -7,6 +7,7 @@ import DiskStorageProvider from '../StorageProvider/implementations/DiskStorageP
 
 interface IRequest {
   filename: string;
+  language: string;
 }
 
 class ProcessImageService {
@@ -16,10 +17,10 @@ class ProcessImageService {
     this.diskStorageProvider = new DiskStorageProvider();
   }
 
-  public async execute({ filename }: IRequest): Promise<string[]> {
+  public async execute({ filename, language }: IRequest): Promise<string[]> {
     const image = path.resolve(__dirname, '..', '..', 'tmp', filename);
 
-    const response = await Tesseract.recognize(image, 'eng');
+    const response = await Tesseract.recognize(image, language);
 
     const phrases = convertoStringToPhrases(response.data.text);
 
